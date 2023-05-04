35aa5d6994270a6e568a3caef87c7c8a3143de02
Lada <lada.goncharova@gmail.com>
2023-05-04 22:51:10 +0300

    ADD user

--- pg-server-app/models/user.js
+++ pg-server-app/models/user.js
@@ -1,5 +1,16 @@
 class User{
-  static create(){}
+  static create({firsName, lastName, email, tel}){
+    //прописати  sql  - запит 
+    //поверне те що створено ^
+    const insertQuery = `
+    INSERT INTO users (first_name, last_name, email, tel)
+    VALUES(${firstName}, ${lastName}, ${email}, ${tel})
+    REURNING *; 
+    `;
+
+    //виконати запит
+    //повернути результат або помилку
+  }
   static getAll(){}
   static getById(){}
   static updateById(){}
